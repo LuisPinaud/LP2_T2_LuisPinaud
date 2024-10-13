fe66bc79af144bde5180e19bf0c4c1f9ef7ea280
Luis Pinaud <luispinaud2005@gmail.com>
2024-10-12 20:55:21 -0500

    Estructura inicial del proyecto

---
+++ src/main/java/pe/com/cibertec/LP2_T2_LuisPinaud/Lp2T2LuisPinaudApplication.java
@@ -0,0 +1,13 @@
+package pe.com.cibertec.LP2_T2_LuisPinaud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lp2T2LuisPinaudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lp2T2LuisPinaudApplication.class, args);
+	}
+
+}
